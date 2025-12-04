aac0b0fcc0770b6647fd0a34f3ae6ab53220be20
s-a-c <71233932+s-a-c@users.noreply.github.com>
2025-12-04 16:26:54 +0000

    init: my first laravel app "bootcamp:chirper"

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+
+    <div class="mx-auto max-w-2xl mt-8">
+        <div class="card bg-base-100 mt-8 shadow">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
+                    <p class="text-base-content/60 mt-4">This is your brand new Laravel application. Time to make it
+                        sing (or chirp)!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
