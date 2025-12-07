de474a66eb07a0f8bba86849c9f827f6e539ba5a
s-a-c <71233932+s-a-c@users.noreply.github.com>
2025-12-07 22:22:30 +0000

    refactor: rename workflow scripts and add banners to workflow test scripts
    - Rename workflow:core to test:core (with testsuite:core as alias)
    - Rename workflow:full to testsuite:full
    - Rename workflow:heavy to testsuite:heavy
    - Rename workflow:policy to policy:checksum-monitor
    - Add banners to workflow:test:lint, workflow:test:pre-commit, workflow:test:browser
    - Update workflow files to use new script names
    - Add declare(strict_types=1) to browser-tests-helper.php

--- scripts/browser-tests-helper.php
+++ scripts/browser-tests-helper.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 function assertNoJavaScriptErrorsExceptCspParser(PendingAwaitablePage|AwaitableWebpage|Webpage $page): PendingAwaitablePage|AwaitableWebpage|Webpage
 {
     try {
